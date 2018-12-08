46606c28e231827031c4551205064ff9e563d5ac
Max Ainatchi <maxwell.ainatchi@gmail.com>
2018-12-08 13:54:30 -0500

    Authorized marketplaceviewcontroller

--- leARn 1.0/Extensions/UIAlertController+error.swift
+++ leARn 1.0/Extensions/UIAlertController+error.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 extension UIAlertController {
-    convenience init(error: Error?) {
+    convenience init(error: Error?, completion: (() -> Void)? = nil) {
         let text = error != nil ? "\(error!)" : "An unknown error occurred. Please try again."
         self.init(title: "An Error Occurred", message: text, preferredStyle: .alert)
-        self.addAction(UIAlertAction(title: "Done", style: .default) { _ in
-            self.dismiss(animated: true, completion: nil)
+        self.addAction(UIAlertAction(title: "Done", style: .default) { [weak self] _ in
+            self?.dismiss(animated: true) {
+                completion?()
+            }
         })
     }
 }
